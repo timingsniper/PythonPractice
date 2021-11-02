8d3ec605140ad70baffc055565b90b292bcc6d9e
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-11-02 13:46:30 +0800

    Create 字符菱形.py

---
+++ 2021练习题/字符菱形.py
@@ -0,0 +1,10 @@
+# Written by Percy Joonwoo Jang, 1800094804
+
+import math
+char = input()
+
+print("  " + char + "  ")
+print(" " + char + char + char + " ")
+print(char+char+char+char+char)
+print(" " + char + char + char + " ")
+print("  " + char + "  ")
