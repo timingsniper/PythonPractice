3cef3aa12099cec1794d74684270af2617320bfa
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-11-20 16:53:03 +0800

    Create 2739 구구단.py

---
+++ Python 101 Sunday/2739 구구단.py
@@ -0,0 +1,11 @@
+# Written by Percy Joonwoo Jang, 1800094804
+
+import math
+
+
+n = int(input())
+counter = 1
+
+while counter<=9:
+    print("%d * %d = %d" %(n, counter, n*counter))
+    counter += 1
