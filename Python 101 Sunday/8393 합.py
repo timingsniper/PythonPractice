9e3111b4d32aaa2c7a2f039de6b0bb3f959a752e
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-11-20 16:59:34 +0800

    Create 8393 합.py

---
+++ Python 101 Sunday/8393 합.py
@@ -0,0 +1,14 @@
+# Written by Percy Joonwoo Jang, 1800094804
+
+import math
+
+
+n = int(input())
+counter = 1
+answer = 0
+
+while counter<=n:
+    answer += counter
+    counter += 1
+
+print(answer)
