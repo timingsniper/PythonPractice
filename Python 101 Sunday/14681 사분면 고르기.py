a6417fb2c31b12f3485255b4897d01cf9fc6a5cc
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-11-20 16:42:42 +0800

    Create 14681 사분면 고르기.py

---
+++ Python 101 Sunday/14681 사분면 고르기.py
@@ -0,0 +1,16 @@
+# Written by Percy Joonwoo Jang, 1800094804
+
+import math
+
+
+x = int(input())
+y = int(input())
+
+if x>0 and y>0:
+    print("1")
+elif x<0 and y>0:
+    print("2")
+elif x<0 and y<0:
+    print("3")
+elif x>0 and y<0:
+    print("4")
